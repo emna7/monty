ba925f542341b82f32d52b822e1ac6dc0bacd7c1
anaruzz <laabidigh@gmail.com>
2020-01-05 02:11:00 +0100

    Changes

--- _pint.c
+++ _pint.c
@@ -5,12 +5,7 @@
 void _pint(stack_t **head, unsigned int line_number)
 {
 if (*head == NULL)
-{
 printf("head is empty instruction in line: %d\n", line_number);
-}
 else
-{
-{
 printf("%d\n", (*head)->n);
-}
 }
