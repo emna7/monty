d361ee274c448287f1a2f8676a9dc668ae5dad87
emna7 <bhmemna7@gmail.com>
2020-01-05 00:52:56 +0000

    pint function

---
+++ _pint.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "monty.h"
+
+void _pint(stack_t **head, unsigned int line_number)
+{
+if (*head == NULL)
+{
+printf("head is empty instruction in line: %d\n", line_number);
+}
+else
+{
+{
+printf("%d\n", (*head)->n);
+}
+}
