c58eb263c3ddf9f3c984eb28aa4121ccd7de5767
emna7 <bhmemna7@gmail.com>
2020-01-05 22:35:13 +0000

    multiplication function

---
+++ _mul.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * _mul - multiplies second top node from top node
+ * @stack: double pointer to top of stack
+ * @line_number: value of node
+ * Return: nothing
+ **/
+void _mul(stack_t **stack, unsigned int line_number)
+{
+int tmp;
+if (*stack == NULL || (*stack)->next == NULL)
+{
+printf("L%u: can't mul, stack too short\n", line_number);
+exit(EXIT_FAILURE);
+}
+tmp = (*stack)->n;
+_pop(stack, line_number);
+(*stack)->n *= tmp;
+}
